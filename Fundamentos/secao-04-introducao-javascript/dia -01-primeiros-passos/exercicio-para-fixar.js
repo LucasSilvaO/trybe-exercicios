e738c4dea54865081e2d405c0f3a63f58e0186a2
LucasSilvaO <lucas.oliveiraaa20@gmail.com>
2022-10-20 16:39:37 -0300

    primeito commit

---
+++ Fundamentos/secao-04-introducao-javascript/dia -01-primeiros-passos/exercicio-para-fixar.js
@@ -0,0 +1,18 @@
+
+// const base = 5
+// const height = 8
+// const area = base * height
+// console.log(area)
+// const perimeter = (base * height) * 2
+// console.log(perimeter)
+
+const nota = 80
+if (nota >= 80){
+    console.log("Parabéns, você foi aprovada(o)!")
+}
+else if (nota <= 80 ){
+    console.log("Você está na nossa lista de espera")
+}
+else if( nota < 60 ){
+    console.log("Você foi reprovado!")
+}
