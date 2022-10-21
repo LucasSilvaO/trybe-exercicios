29c7b42a473e9942a8d0ec5144e2ca2bd9d97d88
LucasSilvaO <lucas.oliveiraaa20@gmail.com>
2022-10-21 15:24:17 -0300

    Adiciona mais exercicios

---
+++ Fundamentos/secao-04-introducao-javascript/dia-01-primeiros-passos/exercicio10.js
@@ -0,0 +1,13 @@
+const valorCusto = 1000
+const valorVenda = 1500
+
+let valorCustoTotal = valorCusto + ( valorCusto * 0.20)
+let lucro = valorVenda - valorCustoTotal
+let vendas = -2
+
+if(vendas === 1000){
+    lucroTotal = lucro * 1000
+    console.log("Seu lucro é ", lucroTotal)
+}else if( valorCusto < 0 || valorVenda < 0 || vendas < 0){
+    console.log("Favor inserir valores maiores que 0.")
+}
---
+++ Fundamentos/secao-04-introducao-javascript/dia-01-primeiros-passos/exercicio6.js
@@ -0,0 +1,19 @@
+let pecaXadrez = "rei"
+
+switch (pecaXadrez) {
+    case "cavalo":
+        console.log("Pula.")
+        
+        break;
+    
+    case "bispo":
+        console.log("Anda em diagonal.")
+        
+        break;
+    
+    case "rei":
+        console.log("Anda uma casa.")
+
+    default:
+        break;
+}
