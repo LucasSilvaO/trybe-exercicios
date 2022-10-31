920803af92a583b68a7c99fcf708168ed1b731fc
LucasSilvaO <lucas.oliveiraaa20@gmail.com>
2022-10-31 19:25:16 -0300

    Adiciona exercicio para fixar

--- Fundamentos/secao-05-javascript-DOM-eventos-e-web-storage/dia-02-javascript-trabalhando-com-documentos/script.js
+++
@@ -1,7 +0,0 @@
-// arquivo script.js
-
-let teste = document.getElementById('start').nextSibling;
-
-console.log(teste) // nó
-
-console.log(document.getElementById('start').nextElementSibling) // <p>elemento</p>
