dcb1fca29a071d38569235e47d88200fc7422e11
LucasSilvaO <lucas.oliveiraaa20@gmail.com>
2023-01-12 22:48:26 -0300

    Termina

--- FrontEnd/secao-01-dia-01-Ambiente-de-Desenvolvimento/gerador-de-senhas/src/main.js
+++ FrontEnd/secao-01-dia-01-Ambiente-de-Desenvolvimento/gerador-de-senhas/src/main.js
@@ -1,1 +1,8 @@
-console.log('Hello World!!')
+import { nanoid } from 'nanoid';
+import './style.css';
+const btn = document.getElementById('btn');
+const senha = document.getElementById('senha');
+
+btn.addEventListener('click', () => {
+    senha.innerHTML = nanoid();
+});
